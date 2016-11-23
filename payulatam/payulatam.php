359e4961287369b47ff76b91450c99fbfd83b7fc
AlvinDiaz <alvin.diaz@payulatam.com>
2016-11-23 15:13:05 -0500

    toolsGetValue

--- payulatam/payulatam.php
+++ payulatam/payulatam.php
@@ -87,7 +87,7 @@ class PayuLatam extends PaymentModule
     public function getContent()
     {
         $html = '';
-        if (!empty($_POST) && !empty($_POST['submitPayU']))
+        if (!empty(Tools::getValue()) && !empty(Tools::getValue('submitPayU')))
         {
             $this->_postValidation();
             if (!count($this->_postErrors))
