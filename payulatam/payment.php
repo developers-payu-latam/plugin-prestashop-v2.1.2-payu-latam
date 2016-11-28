82529b4be1a9224283ab55f7f66d8ccb9ec37293
AlvinDiaz <alvin.diaz@payulatam.com>
2016-11-28 17:35:58 -0500

    se añade funcion para hacer include

--- payulatam/payment.php
+++ payulatam/payment.php
@@ -23,10 +23,15 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-include(dirname(__FILE__) . '/../../config/config.inc.php');
-include(dirname(__FILE__) . '/../../init.php');
-include_once(_PS_MODULE_DIR_ . '/payulatam/payulatam.php');
-
+$dirh = opendir($dirname);
+if ($dirh) {
+    while (($dirElement = readdir($dirh)) !== false) {
+        include(dirname(__FILE__) . '/../../config/config.inc.php');
+        include(dirname(__FILE__) . '/../../init.php');
+        include_once(_PS_MODULE_DIR_ . '/payulatam/payulatam.php');
+    }
+    closedir($dirh);
+}
 class PayUController extends FrontController
 {
     public $ssl = true;
