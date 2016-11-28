33bc26b7f659eaa7a66ae10944e7103a7e345601
AlvinDiaz <alvin.diaz@payulatam.com>
2016-11-28 17:41:29 -0500

    se crea funcion includes

--- payulatam/payment.php
+++ payulatam/payment.php
@@ -23,9 +23,12 @@
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-include(dirname(__FILE__) . '/../../config/config.inc.php');
-include(dirname(__FILE__) . '/../../init.php');
-include_once(_PS_MODULE_DIR_ . '/payulatam/payulatam.php');
+function includes()
+{
+    include(dirname(__FILE__) . '/../../config/config.inc.php');
+    include(dirname(__FILE__) . '/../../init.php');
+    include_once(_PS_MODULE_DIR_ . '/payulatam/payulatam.php');
+}
 
 class PayUController extends FrontController
 {
